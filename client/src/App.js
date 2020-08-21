28886885d25e4b1fd7cb6b0efe9c7d24f108980c
vtyy <h1510134@nushigh.edu.sg>
2020-08-22 04:36:10 +0800

    more buttons

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,18 @@ import {ReactComponent as PrivateUntriggered} from './Assets/Private (Untriggere
 import {ReactComponent as PrivateTriggered} from './Assets/Private (Triggered).svg';
 import {ReactComponent as PublicUntriggered} from './Assets/Public (Untriggered).svg';
 import {ReactComponent as PublicTriggered} from './Assets/Public (Triggered).svg';
+
+import {ReactComponent as CodingUntriggered} from './Assets/Coding untriggered.svg';
+import {ReactComponent as CodingTriggered} from './Assets/Coding triggered.svg';
+import {ReactComponent as DesignUntriggered} from './Assets/Design untriggered.svg';
+import {ReactComponent as DesignTriggered} from './Assets/Design triggered.svg';
+import {ReactComponent as OthersUntriggered} from './Assets/Others untriggered.svg';
+import {ReactComponent as OthersTriggered} from './Assets/Others triggered.svg';
+import {ReactComponent as ProductivityUntriggered} from './Assets/Productivity untriggered.svg';
+import {ReactComponent as ProductivityTriggered} from './Assets/Productivity triggered.svg';
+import {ReactComponent as SciencesUntriggered} from './Assets/Sciences untriggered.svg';
+import {ReactComponent as SciencesTriggered} from './Assets/Sciences triggered.svg';
+
 import {ReactComponent as CreateURL} from './Assets/Create URL (Untriggered).svg';
 
 class URLP extends React.Component{
@@ -20,6 +32,84 @@ class URLP extends React.Component{
     } 
   }
 }
+
+class CodingTag extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {isSelected: false};
+    
+  } 
+  render() {
+    if(this.state.isSelected) { 
+        return (<a onClick={() => this.setState({isSelected: false})}><CodingTriggered/></a>);
+    } else { 
+        return (<a onClick={() => this.setState({isSelected: true})}><CodingUntriggered/></a>);
+    } 
+  }
+}
+
+class DesignTag extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {isSelected: false};
+    
+  } 
+  render() {
+    if(this.state.isSelected) { 
+        return (<a onClick={() => this.setState({isSelected: false})}><DesignTriggered/></a>);
+    } else { 
+        return (<a onClick={() => this.setState({isSelected: true})}><DesignUntriggered/></a>);
+    } 
+  }
+}
+
+class OthersTag extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {isSelected: false};
+    
+  } 
+  render() {
+    if(this.state.isSelected) { 
+        return (<a onClick={() => this.setState({isSelected: false})}><OthersTriggered/></a>);
+    } else { 
+        return (<a onClick={() => this.setState({isSelected: true})}><OthersUntriggered/></a>);
+    } 
+  }
+}
+
+class ProductivityTag extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {isSelected: false};
+    
+  } 
+  render() {
+    if(this.state.isSelected) { 
+        return (<a onClick={() => this.setState({isSelected: false})}><ProductivityTriggered/></a>);
+    } else { 
+        return (<a onClick={() => this.setState({isSelected: true})}><ProductivityUntriggered/></a>);
+    } 
+  }
+}
+
+class SciencesTag extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {isSelected: false};
+    
+  } 
+  render() {
+    if(this.state.isSelected) { 
+        return (<a onClick={() => this.setState({isSelected: false})}><SciencesTriggered/></a>);
+    } else { 
+        return (<a onClick={() => this.setState({isSelected: true})}><SciencesUntriggered/></a>);
+    } 
+  }
+}
+
+
+
 class App extends React.Component{
   constructor(props){
     super(props);
@@ -30,7 +120,7 @@ class App extends React.Component{
 
   render() {
     return (
-      <div class="container" >
+      <div class="container">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"></link>
         <h1 style={{textAlignVertical: "center",textAlign: "center",}}>Create your custom URL now!</h1>
         <p></p>
@@ -59,14 +149,14 @@ class App extends React.Component{
         </div>
         <p></p>
         <p style={{textAlignVertical: "center",textAlign: "center",}}>Add Tags: </p>
-        <div class="btn-group" role="group" style={{display: 'flex', alignItems: "center",}}>
-          <button type="button" class="btn btn-primary">Project Meeting</button>
-          <button type="button" class="btn btn-success">Productivity</button>
+        <div style={{textAlignVertical: "center",textAlign: "center",}}>
+          <SciencesTag/>
+          <ProductivityTag/>
         </div>
-        <div class="btn-group" role="group" style={{display: 'flex', alignItems: "center",}}>
-          <button type="button" class="btn btn-alert"></button>
-          <button type="button" class="btn btn-warning">Webinar</button>
-          <button type="button" class="btn btn-info">Other</button>
+        <div  style={{textAlignVertical: "center",textAlign: "center",}}>
+          <CodingTag/>
+          <DesignTag/>
+          <OthersTag/>
         </div>
         <p/>
         <div style={{textAlignVertical: "center",textAlign: "center",}}><CreateURL/></div>
