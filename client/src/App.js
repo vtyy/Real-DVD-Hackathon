072ca6afc326e8441d13da10b74020b92aa2699c
vtyy <h1510134@nushigh.edu.sg>
2020-08-22 01:36:17 +0800

    UI starting

--- client/src/App.js
+++ client/src/App.js
@@ -20,11 +20,9 @@ class App extends React.Component{
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <p>{this.state.apiResponse}</p>
+      <div class="container">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"></link>
+        <h1>Create your custom URL now!</h1>
       </div>
     );
   }
