f33c18cbae903b8778148c6438051396f6bfc0eb
davidgohzk <david.gohzhekai@gmail.com>
2020-08-22 11:57:03 +0800

    hellowoeld

--- api/routes/setupdatabase.js
+++ api/routes/setupdatabase.js
@@ -6,7 +6,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-   mysqllib.executeQuery('DROP TABLE IF EXISTS url').then(function(){
+   mysqllib.executeQuery('DROP TABLE IF EXISTS url')
+   .then(function(){
       mysqllib.executeQuery('CREATE TABLE url (urlid int NOT NULL AUTO_INCREMENT,originalurl varchar(255) NOT NULL,redirecturl varchar(255) NOT NULL UNIQUE,PRIMARY KEY(urlid))');
    })
    .then(function(){
